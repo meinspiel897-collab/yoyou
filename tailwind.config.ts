1385ab60a540114ae6ea42189ccec6027d39944a
meinspiel897-collab <meinspiel897@gmail.com>
2026-05-25 18:26:12 +0300

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/views/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        accent: "#FC062D",
+        appleLight: {
+          bg: "#FFFFFF",
+          secondaryBg: "#F2F2F7",
+          text: "#000000"
+        },
+        appleDark: {
+          bg: "#000000",
+          secondaryBg: "#1C1C1E",
+          text: "#FFFFFF"
+        }
+      }
+    }
+  },
+  plugins: []
+};
+export default config;
